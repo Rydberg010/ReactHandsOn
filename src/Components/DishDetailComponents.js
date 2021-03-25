a3049ffa982ea23d9fe51d360ef3c9840ea36221
Arunava <arunavabera5@gmail.com>
2021-03-26 00:52:03 +0530

    4

---
+++ src/Components/DishDetailComponents.js
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+
+class DishDetail extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.render() {
+            return (
+                <div className="row">
+                    {DishDetail}
+                </div>
+            )
+        }
+             
+        }
+    }
+}
+
+export default DishDetail;
